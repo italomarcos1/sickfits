951e68c9e0b2cf87821f91803ace16747e8edc45
Italo M <italomarcos0010@gmail.com>
2020-06-07 21:38:23 -0300

    a

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8c9684fe579b200f1315";
+/******/ 	var hotCurrentHash = "4e3de6dbeaf0ce569aa4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
